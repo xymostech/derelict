8e86c126fc0adfd113c7afd8d17987e7d3d834f3
xymostech <xymostech@gmail.com>
2011-12-03 22:02:24 -0500

    Added GLFW setup to tests

--- src/Testmain.cpp
+++ src/Testmain.cpp
@@ -1,5 +1,12 @@
 #include <UnitTest++.h>
 
+#include <GL/glfw.h>
+
 int main() {
+	glfwInit();
+	glfwOpenWindow(5, 5, 0, 0, 0, 0, 16, 0, GLFW_WINDOW);
+
 	return UnitTest::RunAllTests();
+
+	glfwTerminate();
 }
