a428f48b8c1ff85ec1de14d1da1b0369612ae1a4
xymostech <xymostech@gmail.com>
2011-12-25 02:16:08 -0700

    Create WorldObject framework files

---
+++ src/WorldObject.h
@@ -0,0 +1,12 @@
+/*
+ * WorldObject class
+ *
+ * All objects which exist in the world
+ * inherit from this. Defines basic callbacks
+ */
+#ifndef WORLDOBJECT_H
+#define WORLDOBJECT_H
+
+
+
+#endif
